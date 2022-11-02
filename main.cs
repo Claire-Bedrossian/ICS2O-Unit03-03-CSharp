3c23a9ffb85d9f2a5649e6dc0e7638c0206079db
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-02 18:48:09 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,28 @@
+// Created by: Claire Bedrossian
+// Created on: Oct 2022
+//
+// This program calculates the area of a right rectangular pyramid
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double radius;
+        double volume;
+        Console.WriteLine("This program calculates the volume of a sphere.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the length of the radius (cm): ");
+        radius = Convert.ToSingle(Console.ReadLine());
+
+        volume = (4.0/3.0) * Math.PI * Math.Pow(radius,3.00);
+
+        Console.WriteLine("");
+        Console.WriteLine("The volume is: " + volume.ToString("0.000") + " cm³. ");
+
+        Console.WriteLine("\nDone.");
+    }
+}
